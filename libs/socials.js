739ae4c0fc875c44028866f836575ffba1d54ed1
joyboyid <ican.notfound@gmail.com>
2023-03-01 17:46:28 +0800

    add mail contact

--- libs/socials.js
+++ libs/socials.js
@@ -1,25 +1,25 @@
 import {
-    IconBrandFacebook,
-    IconBrandGithub,
-    IconBrandInstagram,
-    IconBrandTwitter,
-    IconMail,
+  IconBrandFacebook,
+  IconBrandGithub,
+  IconBrandInstagram,
+  IconBrandTwitter,
+  IconMail,
 } from '@tabler/icons';
 
 export const socials = [
-    {
-        name: 'Instagram',
-        url: 'https://instagram.com/sundawa.pr',
-        icon: IconBrandInstagram,
-    },
-    {
-        name: 'Facebook',
-        url: 'https://facebook.com/',
-        icon: IconBrandFacebook,
-    },
-    {
-        name: 'joyboy@unram.ac.id',
-        url: 'mailto:joyboy@unram.ac.id',
-        icon: IconMail,
-    },
+  {
+    name: 'Instagram',
+    url: 'https://instagram.com/sundawa.pr',
+    icon: IconBrandInstagram,
+  },
+  {
+    name: 'Facebook',
+    url: 'https://facebook.com/sundawa-pr',
+    icon: IconBrandFacebook,
+  },
+  {
+    name: 'E-Mail',
+    url: 'mailto:sundawasela@gmail.com',
+    icon: IconMail,
+  },
 ];
